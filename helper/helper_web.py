0607838c6e8e42753d71790ac44958a6657ec174
Rahul Mishra <rahulmishra@lambdatest.com>
2026-01-07 16:08:02 +0530

    updated: dependency and fixed tests

--- helper/helper_web.py
+++ helper/helper_web.py
@@ -1,34 +1,46 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.safari.options import Options as SafariOptions
 from helper.helper_base import HelperFunc
 import os
 
-caps = {}
 
 def get_browser(browser, browser_version):
     user_name = os.environ.get("LT_USERNAME")
     access_key = os.environ.get("LT_ACCESS_KEY")
 
-    caps['name'] = "[LambdaTest] [Behave] Testing using Behave and Selenium"
-    caps['build'] = "[LambdaTest] [Behave] Testing using Behave and Selenium"
-    caps['browserName'] = browser
-    caps['version'] = browser_version
-    caps['platform'] = os.environ.get("TARGET_OS")
-    caps['network'] = True
-    caps['visual'] = True
-    caps['video'] = True
-    caps['console'] = True
-    # Accept Insecure Certs (Will not work, details at
-    # https://github.com/SeleniumHQ/selenium/issues/6534#issuecomment-499961981)
-    caps['acceptInsecureCerts'] = True
-    # Known Issue : Selenium Remote WebDriver ignores invalid SSL certificates
-    # https://github.com/SeleniumHQ/selenium/issues/6534#issuecomment-499961981
-    # Even with valid SSL certificate https://www.ssllabs.com/ssltest/analyze.html?d=hub.lambdatest.com&latest, error pops
-    # up
-    ########## Error ################
-    #HTTPSConnectionPool(host='hub.lambdatest.com', port=443): Max retries exceeded with url: /wd/hub/session (Caused by SSLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1129)')))
-    #HOOK-ERROR in after_all: AttributeError: 'Context' object has no attribute 'helperfunc'
-    # Workaround - Disable HTTPS connection and use HTTP connection
-    #remote_url = "http://" + "LT_USER_NAME" + ":" + "LT_ACCESS_KEY" \
-    #            + "@hub.lambdatest.com/wd/hub"
+    # LambdaTest specific capabilities
+    lt_options = {
+        'name': "[LambdaTest] [Behave] Testing using Behave and Selenium",
+        'build': "[LambdaTest] [Behave] Testing using Behave and Selenium",
+        'platformName': os.environ.get("TARGET_OS"),
+        'network': True,
+        'visual': True,
+        'video': True,
+        'console': True,
+    }
+
+    # Select browser-specific options
+    browser_lower = browser.lower()
+    if browser_lower == "chrome":
+        options = ChromeOptions()
+    elif browser_lower == "firefox":
+        options = FirefoxOptions()
+    elif browser_lower in ["edge", "microsoftedge"]:
+        options = EdgeOptions()
+    elif browser_lower == "safari":
+        options = SafariOptions()
+    else:
+        # Default to Chrome options
+        options = ChromeOptions()
+
+    # Set browser version
+    options.browser_version = browser_version
+
+    # Set LambdaTest capabilities using the LT:Options key
+    options.set_capability('LT:Options', lt_options)
+
     remote_url = "https://" + user_name + ":" + access_key + "@hub.lambdatest.com/wd/hub"
-    return HelperFunc(webdriver.Remote(command_executor=remote_url, desired_capabilities=caps))
+    return HelperFunc(webdriver.Remote(command_executor=remote_url, options=options))
